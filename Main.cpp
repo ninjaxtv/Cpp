9ddd9abd16ad3c32aa812c87712c0e0ba7662eaf
ninjaxtv <77315008+ninjaxtv@users.noreply.github.com>
2021-02-24 19:03:22 +0530

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	vector<int> a = {45, 89, 23};
+	sort(a.begin(), a.end()); // To sort the items in vector a in ascesding order
+	a.push_back(1000); // To add 1000 as the last element in vector (vector a)
+	IsThere = binary_search(a.begin(), a.end(), 45); // This will check if in the vector a has 45 or not, The answer will be true
+	 
+    	
+}
---
+++ gender guesser.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string gender;
+cout << "enter your gender: ";
+cin >> gender;
+
+if (gender == "male") {
+    cout << "you are male";
+}
+else if (gender == "female") {
+    cout << "you are female";
+}
+}
